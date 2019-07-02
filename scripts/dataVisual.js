e422f942b75486fb4ca81ce342c8b99f5ea0761e
Ubuntu <ubuntu@ip-172-31-38-227.ec2.internal>
2019-07-02 08:08:23 +0000

    pulling data from API and structuring them into functions

--- scripts/dataVisual.js
+++ scripts/dataVisual.js
@@ -1,2 +1,33 @@
 // Sanity Check
-alert("Hello! I am an alert box!!");
+// alert("Hello! I am an alert box!!");
+
+// Data Gathering from CryptoCompare
+let crypto = null
+/*global axios*/
+axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD', {
+    params: {
+    api_key:"2ccfbedbc83b1a45687c4e6eeaa6ab79299b4ade9398cee3878b6a42c1066f73"
+    }
+  })
+  .then(function (response) {
+      let readings = response.data.Data
+      for(x in readings) {
+      // console.log(readings[x])
+       let name = readings[x].CoinInfo.FullName;
+       let abbrv = readings[x].CoinInfo.Name;
+       let price = readings[x].DISPLAY.USD.PRICE;
+       let infocoin = {name, abbrv, price};
+       console.log(infocoin);
+      }
+      console.log(readings);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+    
+  });  
+
+  
+  
